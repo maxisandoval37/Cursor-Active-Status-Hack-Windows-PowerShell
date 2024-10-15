d606a9a4f49145eb83fa1ecb2319f6e591d092f0
Maxi Sandoval <maxisandoval98@gmail.com>
2024-10-15 14:11:07 -0300

    el script solo corre de lun a vie

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
 Add-Type -AssemblyName 'System.Windows.Forms'
 
-# Definir constantes para la hora de inicio y de fin
+# Constantes para la hora de inicio y de fin
 $startHour = 9
 $endHour = 18
 
@@ -15,11 +15,24 @@ function Write-Log {
 
 function Toggle-ScrollLock {
     [System.Windows.Forms.SendKeys]::SendWait("{SCROLLLOCK}")
-    Write-Log "Precionando: Scroll Lock."
+    Write-Log "Presionando: Scroll Lock."
 }
 
 Write-Log "Script iniciado."
 
+# Verificar que el dia sea de lunes (1) a viernes (5)
+while ($true) {
+    $currentTime = Get-Date
+    $dayOfWeek = $currentTime.DayOfWeek
+    if ($dayOfWeek -ge 1 -and $dayOfWeek -le 5) {
+        Write-Log "Es un dia laboral (lunes a viernes)."
+        break
+    }
+    Write-Log "No es un dia laboral, esperando hasta el próximo día laboral..."
+    Start-Sleep -Seconds 600  # Esperar 10 minutos y volver a comprobar
+}
+
+# Esperar hasta la hora de inicio (9 AM)
 while ($true) {
     Write-Log "Validando hora de inicio..."
     $currentTime = Get-Date
